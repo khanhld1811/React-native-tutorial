35be3d0a05b753f6ef52eb0a66d162782ceff50a
= <=>
2019-12-18 17:37:05 +0700

    12_SectionList

---
+++ 12_SectionList/CodeDemo/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import BasicSectionList from './components/BasicSectionList';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => BasicSectionList);
